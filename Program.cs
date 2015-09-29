7b5d9cdc66cbe3fe8fab7b24c48ae9938f7c45a2
Galiafalt <galiaf.alt@gmail.com>
2015-09-29 10:35:21 +0300

    FirstVersion

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections;
+
+namespace TextAutocomplite
+{
+    class Program
+    {
+        private static Trie TrieDictionary = new Trie();
+        
+        static void Main(string[] args)
+        {
+            Stopwatch WorkTime = new Stopwatch();
+            String InputFile = String.Empty;
+
+            int LinesToRead = 0;
+            while (!File.Exists(InputFile)) InputFile = Console.ReadLine();
+            WorkTime.Start();
+            using (StreamReader ReadFile = new StreamReader(InputFile))
+            {
+                LinesToRead = int.Parse(ReadFile.ReadLine());
+                for (int i = 0; i < LinesToRead; ++i) TrieDictionary.Add(ReadFile.ReadLine());
+                LinesToRead = int.Parse(ReadFile.ReadLine());
+                for (int i = 0; i < LinesToRead; ++i)
+                {
+                    var seg = TrieDictionary.GetSeg(ReadFile.ReadLine());
+                    foreach (var str in seg)
+                    {
+                        Console.WriteLine(str);
+                    }
+                    Console.WriteLine();
+                }
+            }
+            WorkTime.Stop();
+            Console.WriteLine("Время выполнения {0} мин {1} сек {2} милсек", WorkTime.Elapsed.Minutes, WorkTime.Elapsed.Seconds, WorkTime.Elapsed.Milliseconds);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Trie.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.Concurrent;
+using System.IO;
+
+namespace TextAutocomplite
+{
+    public class WordListItem
+    {
+        public String word;
+        public int freq;
+
+        public WordListItem(String str)
+        {
+            var parts = str.Split(' ');
+            word = parts[0];
+            if (parts.Length > 1) freq = int.Parse(parts[1]);
+        }
+    }
+
+    public class Node
+    {
+        public char NodeKey { get; set; }
+        public Dictionary<int, Node> Childs { get; set; }
+        public Dictionary<String, int> Words { get; set; }
+
+        public Node()
+        {
+            Words = new Dictionary<String, int>();
+        }
+    }
+    
+    public class Trie
+    {
+        private Node root;
+
+        public Trie()
+        {
+            root = new Node() { NodeKey = ' '};
+        }
+
+        public void Add(String word)
+        {
+            Node current = root;
+            Node tmp = null;
+
+            var parts = word.Split(' ');
+            foreach (var ch in parts[0])
+            {
+                if (current.Childs == null) { current.Childs = new Dictionary<int, Node>(); }
+                if (!current.Childs.Keys.Contains(ch))
+                {
+                    tmp = new Node();
+                    tmp.NodeKey = ch;
+                    tmp.Words.Add(parts[0], int.Parse(parts[1]));
+                    current.Childs.Add(ch, tmp);
+                }
+                else
+                {
+                    int Sequence = int.Parse(parts[1]);
+                    if (current.Words.Count < 10)
+                    {
+                        current.Words.Add(parts[0], Sequence);
+                    }
+                    else
+                    {
+                        int MinValue = current.Words.Values.Min();
+                        if (MinValue < Sequence)
+                        {
+                            current.Words.Remove(current.Words.Where(r => r.Value == MinValue).OrderByDescending(r => r.Key).Select(r => r.Key).First());
+                            current.Words.Add(parts[0], Sequence);
+                        }
+                    }
+                }
+                current = current.Childs[ch];
+            }
+        }
+
+        public String[] GetSeg(String str)
+        {
+            Node current = root;
+            String[] nullres = new String[0];
+            foreach (var ch in str)
+            {
+                if (current.Childs == null)
+                { return nullres; }
+                else
+                {
+                    if (current.Childs.Keys.Contains(ch))
+                        current = current.Childs[ch];
+                }
+            }
+            return current.Words.OrderByDescending(r => r.Value).ThenBy(r => r.Key).Select(r => r.Key).ToArray();
+        }
+    }
+}
